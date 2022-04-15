bf8267eb5e24fa9d277fd17fc82aefe6c605eaf3
Mash Ok <90055466+mashokk@users.noreply.github.com>
2022-04-15 04:50:07 +0300

    еще не

--- 7 лаба/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ 7 лаба/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -41,6 +41,24 @@ namespace WpfApp1
             null,
             new ValidateValueCallback(isNameValid));
 
+        static Window2ViewModel()
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.CoerceValueCallback = new CoerceValueCallback(CorrectValue);
+        }
+
+        private static object CorrectValue(DependencyObject d, object value)
+        {
+            string val = (string)value;
+
+            if (!string.IsNullOrEmpty(val) &&
+                System.Text.RegularExpressions.Regex.IsMatch(val, "[/!@#?/}[}{]"))
+            {
+                val = "gg";
+                return val;
+            }
+            return value;
+        }
         private static bool isNameValid(object value)
         {
             bool ret = true;
