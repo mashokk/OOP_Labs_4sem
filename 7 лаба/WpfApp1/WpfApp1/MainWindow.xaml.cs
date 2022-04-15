1dbbe4b0f67d5389930c28632695978ecadfca7b
Mash Ok <90055466+mashokk@users.noreply.github.com>
2022-04-15 04:29:53 +0300

    не доделано

---
+++ 7 лаба/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        Window2ViewModel _vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _vm = new Window2ViewModel();
+            this.DataContext = _vm;
+        }
+    }
+
+    class Window2ViewModel : DependencyObject
+    {
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public static DependencyProperty NameProperty = DependencyProperty.Register(
+            "Name",
+            typeof(string),
+            typeof(Window2ViewModel),
+            null,
+            new ValidateValueCallback(isNameValid));
+
+        private static bool isNameValid(object value)
+        {
+            bool ret = true;
+            string val = (string)value;
+
+            if (!string.IsNullOrEmpty(val) &&
+                System.Text.RegularExpressions.Regex.IsMatch(val, "[/!@#?/}[}{]"))
+                ret = false;
+            else
+                ret = true;
+
+            return ret;
+        }
+
+    }
+
+}
